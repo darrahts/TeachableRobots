95d2355f6cb43e1488eb5553471124affc2a5bc8
darrahts <timothy.s.darrah@vanderbilt.edu>
2017-09-20 15:33:59 -0500

    added setup scripts

---
+++ SetupScripts/installTensor.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python3-dev -y
+wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.0.0/tensorflow-1.0.0-cp34-cp34m-linux_armv7l.whl
+sudo pip3 install tensorflow-1.0.0-cp34-cp34m-linux_armv7l.whl
