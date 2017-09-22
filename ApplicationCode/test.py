bce90de508fa83a79ebe580dcbd4fa3dd7365d6e
darrahts <timothy.s.darrah@vanderbilt.edu>
2017-09-22 15:12:03 -0500

    added video capture script

--- ApplicationCode/test.py
+++ ApplicationCode/test.py
@@ -1,1 +1,16 @@
-print("this is a gui application.")
+from VideoStream import *
+
+
+
+if __name__ == "__main__":
+#    demo = Demo(0, 1000, True)
+    #demo.StartRegular()
+#    demo.StartThreaded()
+
+	vs = WebcamVideoStream(0).start()
+	while True:
+		frame = vs.read()
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+		if key == ord("q"):
+			break
