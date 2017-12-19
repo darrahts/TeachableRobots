8709faeb7ee79a3fe567286e44d84120e19b7c5f
darrahts <31658834+darrahts@users.noreply.github.com>
2017-12-19 05:30:51 -0600

    fixed typo

--- WorkingCurrent/RobotCode/Run.sh
+++ WorkingCurrent/RobotCode/Run.sh
@@ -3,7 +3,7 @@
 #   This script starts the RobotControlModule.py (RCM) script and the Flask Web Server (FWS).
 
 
-RCM_PATH=$(pwd)/RobotCjontrolModule.py
+RCM_PATH=$(pwd)/RobotControlModule.py
 FWS_PATH=$(pwd)/FlaskServer/src/server.py
 
 ./RCM_PATH &
