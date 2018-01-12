02b7387c651b05f33c8189de66eaa7474b543974
darrahts <timothy.s.darrah@vanderbilt.edu>
2018-01-12 12:37:26 -0600

    updates for demo

--- ApplicationCode/RobotTracker.py
+++ ApplicationCode/RobotTracker.py
@@ -37,13 +37,14 @@ class GridSpace:
     frame = vs.read()
     frame = imutils.resize(frame, width=640, height=480) 
     frameCenter = ((frame.shape[1] // 2) + 21, (frame.shape[0] // 2) + 11)
-    textArea = np.zeros((frame.shape[0],250,3),dtype=np.uint8)
+    textArea = np.zeros((frame.shape[0],550,3),dtype=np.uint8)
 
     square = np.ndarray([4,2], dtype=int)
     square[0] = [130, 40]
     square[1] = [130, 455]
     square[2] = [550, 455]
     square[3] = [550, 40]
+
 
     def FrameOverlay(self, frame):
         ''' Builds the general output window.
@@ -52,8 +53,8 @@ class GridSpace:
 
             frame: the frame to modify
     '''
-        
-        self.textArea = np.zeros((self.frame.shape[0],250,3),dtype=np.uint8)
+
+        #self.textArea = np.zeros((self.frame.shape[0],250,3),dtype=np.uint8)
         cv2.drawContours(self.frame, [self.square], -1, (0, 255, 0), 2)
         cv2.circle(self.frame, (self.frameCenter[0],self.frameCenter[1]), 15, (0,255,255), 2)
         
@@ -78,15 +79,15 @@ class GridSpace:
             cv2.line(self.frame, (334, k), (344, k), (0, 255, 255), 3)
             k += 36
 
-##        maskk = frame.copy()
-##        for i in range(0, len(frame[0])):
-##            for j in range(0, len(frame)):
-##                if(i < 125 or i > 555 or j < 32 or j > 460):
-##                    maskk[j][i] = (0,0,0)
-##
-##        cv2.bitwise_and(self.maskk, self.frame, self.frame, mask=None)
+        maskk = frame.copy()
+        for i in range(0, len(frame[0])):
+            for j in range(0, len(frame)):
+                if(i < 125 or i > 555 or j < 32 or j > 460):
+                    maskk[j][i] = (0,0,0)
 
-        return
+        cv2.bitwise_and(maskk, self.frame, self.frame, mask=None)
+
+        return 
 
 
     def ProcessFrame(self, frame, low, high):
@@ -105,6 +106,11 @@ class GridSpace:
 
                 The processed frame
     '''
+        #lower = np.array([100,100,100])
+        #upper = np.array([255,255,255])
+        #mask = cv2.inRange(frame, lower, upper)
+        #res = cv2.bitwise_and(frame, frame, mask=mask)
+        #cv2.imshow("result", res)
         
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         color = cv2.inRange(frame, low, high)
@@ -185,6 +191,7 @@ class Robot(GridSpace):
         self.rLoc = (0,0)
         self.goal = (0,0)
         self.goalFound = False
+        self.displayGoals = False
 
         
     def SetGoal(self, goal):
@@ -205,8 +212,8 @@ class Robot(GridSpace):
                 c +=1
 
             self.FrameOverlay(self.frame)
-            #window = np.hstack([self.frame,self.textArea])
-            window = self.frame
+            window = np.hstack([self.frame,self.textArea])
+            #window = self.frame
             cv2.imshow(self.title, window)
 
 ##            if(self.goalFound and c == 10):
@@ -249,19 +256,20 @@ class Robot(GridSpace):
     def FrameOverlay(self, frame):
         super().FrameOverlay(self.frame)
         goal = self.LocationToCoordinates(self.goal)
-        self.DrawGoal(goal)
-        self.DrawGoal(self.LocationToCoordinates((3,2)))
-        self.DrawGoal(self.LocationToCoordinates((-3, 1)))
-        self.DrawGoal(self.LocationToCoordinates((1, 4)))
-        self.rLoc = self.CoordinatesToLocation(self.robot)
+        if(self.displayGoals):
+            self.DrawGoal(goal)
+        #self.DrawGoal(self.LocationToCoordinates((3,2)))
+        #self.DrawGoal(self.LocationToCoordinates((-3, 1)))
+        #self.DrawGoal(self.LocationToCoordinates((1, 4)))
+        #self.rLoc = self.CoordinatesToLocation(self.robot)
             
         cv2.putText(self.frame, "(0,0)", (self.frameCenter[0],self.frameCenter[1]+30), cv2.FONT_HERSHEY_PLAIN, .95, (0,255,250), 2)
-        cv2.putText(self.frame, "(%.1f" % self.rLoc[0] + ", %.1f)" %self.rLoc[1], (self.robot[0]+15,self.robot[1]+30), cv2.FONT_HERSHEY_PLAIN, .95, (50,100,200), 2)
+        #cv2.putText(self.frame, "(%.1f" % self.rLoc[0] + ", %.1f)" %self.rLoc[1], (self.robot[0]+15,self.robot[1]+30), cv2.FONT_HERSHEY_PLAIN, .95, (50,100,200), 2)
         
         #cv2.putText(self.textArea, "Heading: %.2f" % self.ellipse[2], (10, 20), 3, .7, (100,200,100), 1)
-        cv2.circle(self.textArea, (208,8), 2, (100,200,100), 1)
+        #cv2.circle(self.textArea, (208,8), 2, (100,200,100), 1)
 
-        cv2.putText(self.textArea, "Goal: " + str(self.goal), (10, 50), 3, .7, (100,200,100), 1)
+        #cv2.putText(self.textArea, "Goal: " + str(self.goal), (10, 50), 3, .7, (100,200,100), 1)
 
         if(len(self.contour) > 0):
             self.ellipse = cv2.fitEllipse(self.contour)
@@ -269,8 +277,8 @@ class Robot(GridSpace):
             l = self.ellipse[1][1] * 1.25
             cv2.ellipse(self.frame, (self.ellipse[0],(w,l),self.ellipse[2]), (0,255,0), 2)
 
-        if(self.goalFound):
-            cv2.putText(self.textArea, "Goal Found!", (10, 100), 3, .7, (100,200,100), 1)
+        #if(self.goalFound):
+        #    cv2.putText(self.textArea, "Goal Found!", (10, 100), 3, .7, (100,200,100), 1)
         
         return
 
@@ -303,15 +311,6 @@ class Robot(GridSpace):
     def GetHeading(self, frame):
         pass
 
-
-
-if (__name__ == "__main__"):
-
-    r = Robot()
-    r.SetGoal((2,-2))
-    r.Run()
-
-    
 
 
 
