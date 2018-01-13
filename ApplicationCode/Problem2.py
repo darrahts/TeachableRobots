9430766e189f27087c67575c5fc8a9301dc523df
darrahts <timothy.s.darrah@vanderbilt.edu>
2018-01-12 22:53:35 -0600

    problem 2 created.

---
+++ ApplicationCode/Problem2.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+from RobotTracker import *
+import time
+from threading import Thread, Event
+
+
+if (__name__ == "__main__"):
+    
+    r = Robot()
+    #r.SetGoal((2,2))
+    r.displayGoals = False
+
+    repeatCounter = 0
+
+    runThread = Thread(target=r.Run)
+    e = Event()
+    runThread.start()
+    
+    #r.Run()
+
+    ############################################################################################
+    #   drive to point
+
+    r.setGoal((-3,1))
+    cv2.putText(r.textArea, "drive the robot to (-3,1)", (0, 40), 2, .5, (100,200,100), 1)
+    t1 = time.time()
+    while(abs(r.rLoc[0] - r.goal[0]) > .6 and abs(r.rLoc[1] - r.goal[1] > .6)):
+        t2 = time.time()
+        if(t2 - t1 > 60):
+            if(repeatCounter == 0):
+                cv2.putText(r.textArea, "points are given in (x,y) format", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "The point is 3 units to the left of the origin and 1 unit up", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 2):
+                cv2.putText(r.textArea, "Drive to the point shown", (0, 160), 2, .5, (100,200,100), 1)
+                #TODO draw point on frame
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "maybe you need some extra assistance", (0, 160), 2, .5, (100,200,100), 1)
+                x = input()
+                if(x == "continue"):
+                    break
+
+    cv2.putText(r.textArea, "Nice Work!", (0, 300), 4, 1.2, (100,200,100), 1)
+    repeatCounter = 0
+    time.sleep(3)
+    r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
+
+    ############################################################################################
+    #   origin
+
+    r.setGoal((2,-4))
+    cv2.putText(r.textArea, "drive the robot to the point shown", (0, 40), 2, .5, (100,200,100), 1)
+    t1 = time.time()
+    while(abs(r.rLoc[0] - r.goal[0]) > .6 and abs(r.rLoc[1] - r.goal[1] > .6)):
+        t2 = time.time()
+        if(t2 - t1 > 60):
+            if(repeatCounter == 0):
+                cv2.putText(r.textArea, "'x' should be positive, 'y' should be negative", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "'y' = -4", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 2):
+                cv2.putText(r.textArea, "Drive to (2,-4)", (0, 160), 2, .5, (100,200,100), 1)
+                #TODO draw point on frame
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "maybe you need some extra assistance", (0, 160), 2, .5, (100,200,100), 1)
+                x = input()
+                if(x == "continue"):
+                    break
+
+    cv2.putText(r.textArea, "Nice Work!", (0, 300), 4, 1.2, (100,200,100), 1)
+    repeatCounter = 0
+    time.sleep(3)
+    r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
+
+    #e.set()
+    #runThread.join()
+    
+
+    
