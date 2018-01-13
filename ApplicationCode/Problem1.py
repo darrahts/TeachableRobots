2f442bef1126141fb396d6e0d47477444a04a9d1
darrahts <timothy.s.darrah@vanderbilt.edu>
2018-01-12 22:52:58 -0600

    updates for demo

--- ApplicationCode/Problem1.py
+++ ApplicationCode/Problem1.py
@@ -18,7 +18,11 @@ if (__name__ == "__main__"):
     runThread.start()
     
     #r.Run()
-    
+
+    ############################################################################################
+    #   origin
+
+    r.setGoal((0,0))
     cv2.putText(r.textArea, "drive the robot to the origin", (0, 40), 2, .5, (100,200,100), 1)
     t1 = time.time()
     while(abs(r.rLoc[0] - r.goal[0]) > .6 and abs(r.rLoc[1] - r.goal[1] > .6)):
@@ -39,7 +43,160 @@ if (__name__ == "__main__"):
     time.sleep(3)
     r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
 
+    ############################################################################################
+    #   first quadrant
+    
+    r.setGoal((0,0))
+    cv2.putText(r.textArea, "drive the robot into the first quadrant.", (0, 40), 2, .5, (100,200,100), 1)
+    t1 = time.time()
+    while(abs(r.rLoc[0] - r.goal[0]) < .6 and abs(r.rLoc[1] - r.goal[1] < .6)):
+        t2 = time.time()
+        if(t2 - t1 > 60):
+            if(repeatCounter == 0):
+                cv2.putText(r.textArea, "the first quadrant is top-right.", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "maybe you need some extra assistance", (0, 160), 2, .5, (100,200,100), 1)
+                x = input()
+                if(x == "continue"):
+                    break
 
+    cv2.putText(r.textArea, "Awesome!", (0, 300), 4, 1.2, (100,200,100), 1)
+    repeatCounter = 0
+    time.sleep(3)
+    r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
+
+    ############################################################################################
+    #   x axis
+    
+    r.setGoal((5,0))
+    cv2.putText(r.textArea, "drive the robot to any point on the 'X' axis", (0, 40), 2, .5, (100,200,100), 1)
+    t1 = time.time()
+    while(abs(r.rLoc[1] - r.goal[1]) > .6):
+        t2 = time.time()
+        if(t2 - t1 > 60):
+            if(repeatCounter == 0):
+                cv2.putText(r.textArea, "the 'X' axis is horizontal.", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "maybe you need some extra assistance", (0, 160), 2, .5, (100,200,100), 1)
+                x = input()
+                if(x == "continue"):
+                    break
+
+    cv2.putText(r.textArea, "Great!", (0, 300), 4, 1.2, (100,200,100), 1)
+    repeatCounter = 0
+    time.sleep(3)
+    r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
+
+    ############################################################################################
+    #   y axis
+    
+    r.setGoal((0,5))
+    cv2.putText(r.textArea, "drive the robot to any point on the 'y' axis", (0, 40), 2, .5, (100,200,100), 1)
+    t1 = time.time()
+    while(abs(r.rLoc[0] - r.goal[0]) > .6):
+        t2 = time.time()
+        if(t2 - t1 > 60):
+            if(repeatCounter == 0):
+                cv2.putText(r.textArea, "the 'Y' axis is vertical.", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "maybe you need some extra assistance", (0, 160), 2, .5, (100,200,100), 1)
+                x = input()
+                if(x == "continue"):
+                    break
+
+    cv2.putText(r.textArea, "Good Job!", (0, 300), 4, 1.2, (100,200,100), 1)
+    repeatCounter = 0
+    time.sleep(3)
+    r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
+
+    ############################################################################################
+    #   pos or neg y values
+    
+    negY = True
+    r.setGoal((0,0))
+    if(r.rLoc[1] > 0):
+        cv2.putText(r.textArea, "drive the robot to an area with negative 'y' values", (0, 40), 2, .5, (100,200,100), 1)
+    else:
+        cv2.putText(r.textArea, "drive the robot to an area with positive 'y' values", (0, 40), 2, .5, (100,200,100), 1)
+        negY = False
+        
+    t1 = time.time()
+
+    expression = ""
+    if(negY):
+        expression = "r.rLoc[1] > 0"
+    else:
+        expression = "r.rLoc[1] < 0"
+        
+    while(eval(expression)):
+        t2 = time.time()
+        if(t2 - t1 > 60):
+            if(repeatCounter == 0):
+                if(negY):
+                    cv2.putText(r.textArea, "positive 'y' values are above the origin.", (0, 160), 2, .5, (100,200,100), 1)
+                else:
+                    cv2.putText(r.textArea, "negative 'y' values are below the origin.", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "maybe you need some extra assistance", (0, 160), 2, .5, (100,200,100), 1)
+                x = input()
+                if(x == "continue"):
+                    break
+
+    cv2.putText(r.textArea, "Excellent!", (0, 300), 4, 1.2, (100,200,100), 1)
+    repeatCounter = 0
+    time.sleep(3)
+    r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
+
+    ############################################################################################
+    #   pos x and pos/neg y
+    
+#TODO finish this for random quadrant
+    negY = True
+    r.setGoal((0,0))
+    if(r.rLoc[1] > 0):
+        cv2.putText(r.textArea, "drive the robot to an area with negative 'y' values", (0, 40), 2, .5, (100,200,100), 1)
+    else:
+        cv2.putText(r.textArea, "drive the robot to an area with positive 'y' values", (0, 40), 2, .5, (100,200,100), 1)
+        negY = False
+        
+    t1 = time.time()
+
+    expression = ""
+    if(negY):
+        expression = "r.rLoc[1] > 0"
+    else:
+        expression = "r.rLoc[1] < 0"
+        
+    while(eval(expression)):
+        t2 = time.time()
+        if(t2 - t1 > 60):
+            if(repeatCounter == 0):
+                if(negY):
+                    cv2.putText(r.textArea, "positive 'y' values are above the origin.", (0, 160), 2, .5, (100,200,100), 1)
+                else:
+                    cv2.putText(r.textArea, "negative 'y' values are below the origin.", (0, 160), 2, .5, (100,200,100), 1)
+                t1 = t2
+                repeatCounter += 1
+            if(repeatCounter == 1):
+                cv2.putText(r.textArea, "maybe you need some extra assistance", (0, 160), 2, .5, (100,200,100), 1)
+                x = input()
+                if(x == "continue"):
+                    break
+
+    cv2.putText(r.textArea, "Well Done!", (0, 300), 4, 1.2, (100,200,100), 1)
+    repeatCounter = 0
+    time.sleep(3)
+    r.textArea = np.zeros((r.frame.shape[0],550,3),dtype=np.uint8)
+
+    ############################################################################################
 
 
     #e.set()
@@ -50,28 +207,11 @@ if (__name__ == "__main__"):
 
     '''
 
-    cv2.putText(r.textArea, "drive the robot to any point on the 'X' axis", (0, 60), 2, .5, (100,200,100), 1)
-    cv2.putText(r.textArea, "drive the robot to any point on the 'Y' axis", (0, 80), 2, .5, (100,200,100), 1)
-
-    cv2.putText(r.textArea, "drive the robot to an area with negative 'Y' values", (0, 100), 2, .5, (100,200,100), 1)
     cv2.putText(r.textArea, "drive the robot to an area with both positive 'X' and 'Y' values", (0,120), 2, .5, (100,200,100), 1)
     
-
-    cv2.putText(r.textArea, "the origin is where the 'X' and 'Y' axis intersect.", (0, 160), 2, .5, (100,200,100), 1)
-    cv2.putText(r.textArea, "the 'X' axis is horizontal, try again.", (0, 180), 2, .5, (100,200,100), 1)
-    cv2.putText(r.textArea, "the 'Y' axis is horizontal, try again.", (0, 200), 2, .5, (100,200,100), 1)
     
-    cv2.putText(r.textArea, "positive 'Y' values are above the origin... (try again)", (0, 220), 2, .5, (100,200,100), 1)
     cv2.putText(r.textArea, "if negative 'X' values are left of the origin... (try again)", (0, 240), 2, .5, (100,200,100), 1)
     
-
-    cv2.putText(r.textArea, "Nice Work!", (0, 300), 4, 1.2, (100,200,100), 1)
-    cv2.putText(r.textArea, "Great!", (0, 340), 4, 1.2, (100,200,100), 1)
-    cv2.putText(r.textArea, "Good Job!", (0, 380), 4, 1.2, (100,200,100), 1)
-    cv2.putText(r.textArea, "Excellent!", (0, 420), 4, 1.2, (100,200,100), 1)
-    cv2.putText(r.textArea, "Well Done!", (0, 460), 4, 1.2, (100,200,100), 1)
-
-
 '''
 
 
--- ApplicationCode/RobotTracker.py
+++ ApplicationCode/RobotTracker.py
@@ -54,7 +54,6 @@ class GridSpace:
             frame: the frame to modify
     '''
 
-        #self.textArea = np.zeros((self.frame.shape[0],250,3),dtype=np.uint8)
         cv2.drawContours(self.frame, [self.square], -1, (0, 255, 0), 2)
         cv2.circle(self.frame, (self.frameCenter[0],self.frameCenter[1]), 15, (0,255,255), 2)
         
@@ -254,7 +253,7 @@ class Robot(GridSpace):
             return -1
 
 
-    def FrameOverlay(self, frame, msg, loc):
+    def FrameOverlay(self, frame, msg, loc): #TODO draw point, student name in text area
         super().FrameOverlay(self.frame)
         goal = self.LocationToCoordinates(self.goal)
         if(self.displayGoals):
@@ -262,7 +261,7 @@ class Robot(GridSpace):
         #self.DrawGoal(self.LocationToCoordinates((3,2)))
         #self.DrawGoal(self.LocationToCoordinates((-3, 1)))
         #self.DrawGoal(self.LocationToCoordinates((1, 4)))
-        #exitself.rLoc = self.CoordinatesToLocation(self.robot)
+        #self.rLoc = self.CoordinatesToLocation(self.robot)
             
         cv2.putText(self.frame, "(0,0)", (self.frameCenter[0],self.frameCenter[1]+30), cv2.FONT_HERSHEY_PLAIN, .95, (0,255,250), 2)
         #cv2.putText(self.frame, "(%.1f" % self.rLoc[0] + ", %.1f)" %self.rLoc[1], (self.robot[0]+15,self.robot[1]+30), cv2.FONT_HERSHEY_PLAIN, .95, (50,100,200), 2)
