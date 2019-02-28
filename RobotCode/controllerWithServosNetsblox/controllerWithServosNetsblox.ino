b752c84b10e7088fdcbeac2c17de6d3efefdc01e
darrahts <timothy.s.darrah@vanderbilt.edu>
2019-02-28 11:14:09 -0600

    updates

--- RobotCode/controllerWithServosNetsblox/controllerWithServosNetsblox.ino
+++ RobotCode/controllerWithServosNetsblox/controllerWithServosNetsblox.ino
@@ -1,8 +1,15 @@
 #include <Servo.h>
 
-Servo MTR_A;
-Servo MTR_B;
+Servo MTR_L;
+Servo MTR_R;
 
+//line sensors
+#define LEFT_IR A3
+#define MIDDLE_IR A0
+#define RIGHT_IR A2
+
+//voltage sensor
+#define VOLTAGE_SR A1
 
 //min and max velocity vals
 int minV = -20; 
@@ -13,6 +20,36 @@ int minS = 30;
 int maxS = 150;
 
 int currentSpeed = 0;
+int amountL = 0;
+int amountR = 0;
+
+//battery voltage
+double voltage = 0.0;
+
+int dir = 0;
+
+
+//IR baseline readings. adjust these to your sensors and environment
+int readings[] = {500,400,700};
+
+
+
+void CheckVoltage()
+{
+    double v = (analogRead(VOLTAGE_SR) * 5.0) / 1024.0;
+    voltage = (v * 10500.0 / 1500.0);
+    Serial.write('~');
+    delay(50);
+    Serial.println(voltage);
+}
+
+void ReadLineSensors()
+{
+    readings[0] = analogRead(LEFT_IR);
+    readings[1] = analogRead(MIDDLE_IR);
+    readings[2] = analogRead(RIGHT_IR);
+}
+
 
 
 /*
@@ -25,25 +62,36 @@ int currentSpeed = 0;
 void Ramp(int i)
 {
     int scale = 3;
-    
-    MTR_A.write(90+scale*i);
-    MTR_B.write(90-scale*i);
+    amountL = 92-scale*i;
+    amountR = 93+scale*i;
+    if(dir == 1)
+    {
+        MTR_L.write(amountL);
+        MTR_R.write(amountR);
+    }
+    else if(dir == 2)
+    {
+        MTR_L.write(91-scale*i);
+        MTR_R.write(89+scale*i);
+    }
     delay(35);   
 }
 
 void RampL(int i)
 {
     int scale = 3;
-    MTR_A.write(90+scale*i);
-    MTR_B.write(90+scale*i);
-    delay(35); 
+    if(dir == 1)
+    {
+        MTR_L.write(amountL-2-scale*i);
+        MTR_R.write(amountR+scale*i);
+    }
 }
 
 void RampR(int i)
 {
     int scale = 3;
-    MTR_A.write(90-scale*i);
-    MTR_B.write(90-scale*i);
+    MTR_L.write(90-scale*i);
+    MTR_R.write(90-scale*i);
     delay(35); 
 }
 
@@ -72,6 +120,7 @@ void Stop()
         Ramp(0);
     }
     currentSpeed = 0;
+    dir = 0;
 }
 
 
@@ -79,8 +128,23 @@ void Stop()
 /*
  * turns left or right
  */
-
-
+void Left(int amount)
+{
+    int curDir = dir;
+    int curSpd = currentSpeed;
+    dir = 3;
+    if(currentSpeed != 0)
+    {
+        int newAmount = amountL+amount;
+        Serial.println(newAmount);
+        MTR_L.write(newAmount);
+        //MTR_R.write(amountR-amount);
+    }
+    dir = curDir;
+    Serial.println(amountL);
+    MTR_L.write(amountL);
+    MTR_L.write(amountR);
+}
 
 
 
@@ -96,28 +160,41 @@ void Drive(int velocity)
         return;
     }
     
-    //forward and increasing or backward and decreasing
+    //forward and increasing
     if((currentSpeed >= 0 && currentSpeed < velocity) || (velocity <= 0 && velocity > currentSpeed))
     {
+        dir = 1;
         for(int i = currentSpeed+1; i <= velocity; i++) 
         {
             Ramp(i);
         }
     }
 
-    //forward and decreasing or backward and increasing
-    else if((velocity >= 0 && velocity < currentSpeed) || (currentSpeed <= 0 && currentSpeed > velocity))
+    //backward and increasing
+    else if((velocity >= 0 && velocity < currentSpeed)) // || (currentSpeed <= 0 && currentSpeed > velocity))
     {
+        dir = 1;
         for(int i = currentSpeed-1; i >= velocity; i--)
         {
             Ramp(i);
         }
     }
 
+    
+    else if((currentSpeed <= 0 && currentSpeed > velocity))
+    {
+        dir = 2;
+        for(int i = currentSpeed-1; i >= velocity; i--)
+        {
+            Ramp(i);
+        }      
+    }
+
     //forward to backward
     else if(currentSpeed > 0 && velocity < 0)
     {
         Stop();
+        dir = 2;
         for(int i = -1; i >= velocity; i--)
         {
             Ramp(i);
@@ -128,15 +205,16 @@ void Drive(int velocity)
     else if(currentSpeed < 0 && velocity > 0)
     {
         Stop();
+        dir = 1;
         for(int i = 1; i <= velocity; i++)
         {
             Ramp(i);
         }
     }
-    else
-    {
-        Stop();
-    }
+//    else
+//    {
+//        Stop();
+//    }
     currentSpeed = velocity;
 }
 
@@ -144,9 +222,18 @@ void Drive(int velocity)
 //***********************************************************
 void setup() 
 {
-    MTR_A.attach(9);
-    MTR_B.attach(10);
-    Serial.begin(38400);
+    MTR_L.attach(9);
+    MTR_R.attach(10);
+
+    //configure line following sensors
+    pinMode(LEFT_IR, INPUT);
+    pinMode(MIDDLE_IR, INPUT);
+    pinMode(RIGHT_IR, INPUT);
+
+    //configure voltage sensor reading
+    pinMode(VOLTAGE_SR, INPUT);
+    
+    Serial.begin(115200);
     
 }
 
@@ -155,31 +242,53 @@ void setup()
 void loop() 
 {
 
-//    Drive(-10);
+ //   Drive(-5);
+    Serial.println(dir);
+    Serial.println(amountL);
+    Serial.println(amountR);
+ //   MTR_L.write(109);
+ //   MTR_R.write(107);
+    
+    //Serial.println(currentSpeed);
+    delay(1000);
+    //Left(7);
+    
+//    ReadLineSensors();
+//    for(int i = 0; i < 3; i++)
+//    {
+//        Serial.print(readings[i]); Serial.print('\t');
+//    }
+//    Serial.println();
+//    delay(500);
+//      Stop();
+//    Drive(-7);
+//    Serial.println(dir);
 //    delay(1000);
-//    Drive(10);
+//    Drive(-3);
+//    Serial.println(dir);
 //    delay(1000);
-    Stop();
-    delay(2000);
+//    Drive(3);
+//    Serial.println(dir);
+//    delay(1000);
+//    Drive(7);
+//    Serial.println(dir);
+//    delay(1000);
+//    Drive(-5);
+//    Serial.println(dir);
+//    delay(1000);
+//    Stop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- TESTING/sender.py
+++ TESTING/sender.py
@@ -1,12 +1,15 @@
 import base64
 import cv2
 import zmq
+import time
 
 context = zmq.Context()
 footage_socket = context.socket(zmq.PUB)
 footage_socket.connect('tcp://localhost:5555')
 
-camera = cv2.VideoCapture(0)  # init the camera
+camera = cv2.VideoCapture("sampleVideo.mp4")  # for the video
+#camera = cv2.VideoCapture(-1)  # for the camera
+
 
 while True:
     try:
@@ -15,6 +18,7 @@ while True:
         encoded, buffer = cv2.imencode('.jpg', frame)
         jpg_as_text = base64.b64encode(buffer)
         footage_socket.send(jpg_as_text)
+        time.sleep(.02)
 
     except KeyboardInterrupt:
         camera.release()
